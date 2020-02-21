4ce60d7b19f36457649dc0636b8ad0a3cbe2382f
Daniel Paiva <danielcspaiva@gmail.com>
2020-02-21 20:21:46 -0300

    until iteration 4

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,12 +1,114 @@
-/* eslint no-restricted-globals: 'off' */
+// // /* eslint no-restricted-globals: 'off' */
 
-// Iteration 1: Ordering by year - Order by year, ascending (in growing order)
+let movies = [
+    {
+        "title": "The Godfather",
+        "year": 1972,
+        "director": "Francis Ford Coppola",
+        "duration": "2h 55min",
+        "genre": [
+          "Crime",
+          "Drama"
+        ],
+        // "rate": 9.2
+    },
+    {
+      "title": "The Shawshank Redemption",
+      "year": 1994,
+      "director": "Frank Darabont",
+      "duration": "2h 22min",
+      "genre": [
+        "Crime",
+        "Drama"
+      ],
+      "rate": 9.3
+    },
+    {
+      "title": "The Godfather: Part II",
+      "year": 1972,
+      "director": "Francis Ford Coppola",
+      "duration": "3h 22min",
+      "genre": [
+        "Crime",
+        "Drama"
+      ],
+      "rate": 9
+    },
+    {
+        "title": "Schindler\"s List",
+        "year": 1993,
+        "director": "Steven Spielberg",
+        "duration": "3h 15min",
+        "genre": [
+          "Biography",
+          "Drama",
+          "History"
+        ],
+        "rate": 8.9
+      }
+]
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+// // let numbers = [2, 4, 3, 2, 1];
+// // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
-// Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+const orderByYear = (arr) => {
+    let newArray = [...arr];
+    return newArray.sort((a, b) => (a.year > b.year) ? 1 : (a.year === b.year) ? ((a.title > b.title) ? 1 : -1) : -1 );
+};
 
+// // console.log(orderByYear(movies));
+// // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+
+const howManyMovies = (arr) => {
+    let arrSteven = [...arr];
+    // console.log(arrSteven);
+    const arr2 = arrSteven.filter(movie => movie.director === "Steven Spielberg" && movie.genre.includes('Drama'))
+    return arr2.length;
+}
+
+
+// // console.log(howManyMovies(movies));
+// // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+const orderAlphabetically = (arr) => {
+    let newArray = [...arr];
+    // console.log(newArray);
+    newArray = newArray.sort((a, b) => (a.title > b.title) ? 1 : -1 );
+    let alphabeticArray = [];
+    newArray.forEach((movie, idx) => {
+        if (idx < 20) {
+            alphabeticArray.push(movie.title);
+        }
+    })
+    return alphabeticArray;
+}
+
+
+// console.log(orderAlphabetically(movies));
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
+
+const ratesAverage = (arr) => {
+    let notRated = 0;
+    let sum = arr.reduce((acc, movie) => {
+        if (!movie.hasOwnProperty('rate')) {
+            notRated += 1;
+            // console.log(notRated);
+            return acc;
+        }
+        return acc + movie.rate;
+    }, 0);
+    // console.log(sum);
+    if (arr.length === 0) {
+        return 0;
+    }
+     num = (sum / (arr.length - notRated)).toFixed(2);
+     return parseInt(num)
+}
+  
+
+ console.log(ratesAverage(movies));
+
+
 
 // Iteration 5: Drama movies - Get the average of Drama Movies
 
